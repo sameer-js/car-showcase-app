85df70050523f394209efc70b5f610af3d44e12a
Sameer <meersj@gmail.com>
2024-02-11 14:42:08 +0545

    implemented all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,55 @@
-import { Hero } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components';
+import { fuels, yearsOfProduction } from '@/constants';
+import { HomeProps } from '@/types';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    model: searchParams.model || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+  });
+  // console.log(allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className='overflow-hidden'>
       <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard key={car.year + Math.random()} car={car} />
+              ))}
+            </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
