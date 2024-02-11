f3e0931635873f42e4f95c3b47caecff55201fdf
Sameer <meersj@gmail.com>
2024-02-11 14:43:12 +0545

    added component exports

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,12 @@
+import NavBar from "./NavBar";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
 import Footer from "./Footer";
 
-export {NavBar, Hero, CustomButton, Footer}
+export {NavBar, Hero, CustomButton, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails, ShowMore, Footer}
