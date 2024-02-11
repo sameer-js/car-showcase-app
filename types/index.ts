8d2d2e820e671188a8bccc8f3f4e4d3c3587becf
Sameer <meersj@gmail.com>
2024-02-11 14:42:49 +0545

    added interfaces for props

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,62 @@ export interface CustomButtonProps {
     title: string;
     btnType?: "button" | "submit";
     containerStyles?: string;
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 
+}
+
+export interface SearchManufacturerProps{
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps{
+    city_mpg:number;
+    class:string;
+    combination_mpg:number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year:number;
+}
+
+export interface CarDetailsProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps
+}
+
+export interface FilterProps {
+    manufacturer:string;
+    model:string;
+    year: number;
+    fuel:string;
+    limit:number;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+  }
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
 }
