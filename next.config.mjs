d53084d08143e4fca05e2bea629ccfcd5dd3e553
Sameer <meersj@gmail.com>
2024-02-11 14:39:59 +0545

    added images domain hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'cdn.imagin.studio',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
